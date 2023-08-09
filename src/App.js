4a2dd224240c1767ad29c9d38638b186c6e9675e
Atul Joshi <atuljoshi@Atuls-MacBook-Pro.local>
2023-08-09 08:50:08 +0530

    add routers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import User from "./users/pages/User";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<User />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
